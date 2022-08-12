742e27905ff9260c1f6c7fa53ed3ee4f7a65fc60
Your Name <alwinjoby44@gmail.com>
2022-08-12 18:36:23 +0530

    first commit of zom

---
+++ zomjav.js
@@ -0,0 +1,19 @@
+function searchedemail(){
+    
+    let y=document.getElementById("email").value
+
+   console.log("email btn "+ y);
+   document.getElementById("form1").placeholder=y;
+   
+}
+
+function searchedphone(){
+   let x=document.getElementById("Phone").value
+
+   console.log("phonebtn "+x);
+   document.getElementById("form1").placeholder=x;
+}
+
+function isname(){
+    
+}
